65a52017ce805fce6cc384df966a62bba0526d2e
Emani Mahathi <2420030093@klh.edu.in>
2025-08-02 14:29:39 +0530

    Add files via upload

---
+++ sampleprograms/Q5.java
@@ -0,0 +1,16 @@
+package sampleprograms;
+
+public class Q5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int expr1 = 10 + 2 * 5;
+	     int expr2 = (10 + 2) * 5;
+	     int expr3 = 100 / 5 + 2 * 3 - 4;
+
+	     System.out.println("10 + 2 * 5 = " + expr1);
+	     System.out.println("(10 + 2) * 5 = " + expr2);
+	     System.out.println("100 / 5 + 2 * 3 - 4 = " + expr3);
+	}
+
+}
---
+++ sampleprograms/Q8.java
@@ -0,0 +1,24 @@
+package sampleprograms;
+import java.util.*;
+public class Q8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter first number: ");
+	        int a = sc.nextInt();
+	        System.out.print("Enter second number: ");
+	        int b = sc.nextInt();
+
+	        System.out.println("Addition: " + (a + b));
+	        System.out.println("Subtraction: " + (a - b));
+	        System.out.println("Multiplication: " + (a * b));
+
+	        if (b != 0) {
+	            System.out.println("Division: " + (a / b));
+	            System.out.println("Modulus: " + (a % b));
+	        } else {
+	            System.out.println("Division and Modulus by zero are not allowed.");
+	}
+	}
+}
